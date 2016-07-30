b99e601901d5ea446cffb94ee923aacffffe9ba8
sbtjavaschool <sbt.java.school@gmail.com>
2016-07-30 11:28:08 +0300

    .

---
+++ src/ru/sbt/generics/ClassCastExceptionExample.java
@@ -0,0 +1,14 @@
+package ru.sbt.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassCastExceptionExample {
+    public static void main(String[] args) {
+        CollectionSimpleExample example = new CollectionSimpleExample();
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        example.process(list);
+    }
+}
---
+++ src/ru/sbt/generics/CollectionSimpleExample.java
@@ -0,0 +1,10 @@
+package ru.sbt.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionSimpleExample {
+    public void process(List<Integer> list) {
+        Integer o = list.get(0);
+    }
+}
